ccb924620689701669f58b8fa53723e663df2e34
Luqman <luqmannablan@gmail.com>
2025-10-13 14:33:45 -0500

    create project database

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Project(models.Model):
+
+    company = models.CharField(max_length=75)
+    description = models.TextField()
+    skills = models.JSONField(default=list)
+    slug = models.SlugField()
+    date = models.DateField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.company
